ca9a5c21d011a782b3f9404056682a134a093721
Billy Torres <116312457+BRivasTorres@users.noreply.github.com>
2023-06-06 15:59:08 +0000

    Primera manera de utilizar LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./styles.css";
-
+import {useState, useEffect} from "react"
 export default function App() {
+  const [data, setData] = useState("")
+  
+  useEffect(() => {
+    const datos = window.localStorage.getItem("My data")
+    if (datos !== null) setData(JSON.parse(datos))
+  }, [])
+  
+  useEffect(() => {
+    window.localStorage.setItem("My data", JSON.stringify(data))
+  }, [data])
+  
   return (
     <div className="App">
-      <input type="text" placeholder="Enter your name" />
-      <button>Gurdar</button>
+      <input 
+        type="text" 
+        placeholder="Enter your name" 
+        value={data} 
+        onChange={e => setData(e.target.value)} 
+      />
+      <button onClick={() => setData(data)}>Gurdar</button>
     </div>
   );
 }
