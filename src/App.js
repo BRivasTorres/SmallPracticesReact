21938169626a58847f6d189adbc7fb487cc4e23c
Billy Torres <116312457+BRivasTorres@users.noreply.github.com>
2023-06-06 09:31:23 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Enter your name" />
+      <button>Gurdar</button>
+    </div>
+  );
+}
