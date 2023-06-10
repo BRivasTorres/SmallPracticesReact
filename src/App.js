5920f855d25febe4901f3c017286242f153dc19a
Billy Torres <116312457+BRivasTorres@users.noreply.github.com>
2023-06-10 22:17:42 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./styles.css";
-import {useState, useEffect} from "react"
-import {useLocalStorage} from "./useLocalStorage"
+import { useLocalStorage } from "./useLocalStorage";
 export default function App() {
-  const [text, setText] = useLocalStorage("key", "")
+  const [text, setText] = useLocalStorage("key", "");
   const handleSave = (e) => {
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+    console.log(text);
+  };
   return (
     <div className="App">
-      <input 
-        type="text" 
-        placeholder="Enter your name" 
-        value={text} 
-        onChange={e => setText(e.target.value)} 
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
       <button onClick={handleSave}>Gurdar</button>
     </div>
