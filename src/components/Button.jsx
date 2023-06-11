664e1ba9fde980e95052325a79cf3a5e9b9d6702
Billy Torres <116312457+BRivasTorres@users.noreply.github.com>
2023-06-11 21:12:27 +0000

    Componentes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,10 @@ import { useLocalStorage } from "../useLocalStorage";
 
 const Button = () => {
   const [text, setText] = useState("");
+  const [inputs, setInputs] = useState({
+    input1: { text: "" },
+    input2: { text: "" },
+  });
   const [valueSaved, setValueSaved] = useLocalStorage("key", "");
 
   useEffect(() => {
@@ -13,13 +17,22 @@ const Button = () => {
     setValueSaved(text);
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: { text: value },
+    }));
+    console.log(name, value);
+  };
+
   return (
     <div>
       <input
         type="text"
         placeholder="Enter your name"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleChange}
       />
       <button onClick={handleSave}>Guardar</button>
     </div>
